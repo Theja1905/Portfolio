77a2c0064be87bd0753ae0c83bbd912a0eb6701e
Theja1905 <153060692+Theja1905@users.noreply.github.com>
2025-12-10 12:56:07 +0800

    .

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -80,7 +80,7 @@ export default function Projects() {
             Here are some of my recent projects that showcase my skills in full-stack development, design, and problem-solving.
           </p>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 w-full justify-center px-8 ml-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-0 w-full justify-center px-8 ml-5">
               {projects.map((project, index) => (
                 <div
                   key={index}
